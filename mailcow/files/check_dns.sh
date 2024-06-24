3e971da0fa50ee91eb538c2431d5a18c1f3d7da4
Andreas Dvorak <a_github@dvorak.xyz>
2024-06-24 17:42:39 +0200

    dns check

---
+++ mailcow/files/check_dns.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# check the reverse dns
+
+### variables ###
+dns="mail.privatundfrei.de"
+servicedns="DNS"
+servicereversedns="DNS_reverse"
+
+### main ###
+ipaddress=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+#### dns
+nslookup mail.privatundfrei.de |grep ${ipaddress} > /dev/null 2>&1
+
+if [ "$?" != "0" ]
+then
+  status=1
+  statustext="wrong dns"
+else
+  status=0
+  statustext="correct dns"
+fi
+
+echo "$status $servicedns - $statustext"
+
+#### reverse dns
+host ${ipaddress} | grep ${dns} > /dev/null 2>&1
+
+if [ "$?" != "0" ]
+then
+  status=1
+  statustext="wrong reverse dns"
+else
+  status=0
+  statustext="correct reverse dns"
+fi
+
+echo "$status $servicereversedns - $statustext"
